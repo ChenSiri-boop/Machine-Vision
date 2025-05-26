486a3f24618eb7a92335363c88bef2659b0fb7b1
ChenSiri-boop <1768256469@qq.com>
2025-05-26 13:27:01 +0800

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,4 +0,0 @@
-import os,sys
-sys.path.append(os.path.dirname(__file__))
-
-import dataset,models,utils
--- modules/utils/calc.py
+++
@@ -1,8 +0,0 @@
-import torch,torch.nn as nn
-def cos_sim(vector)->torch.Tensor:
-    return  \
-        nn.functional.cosine_similarity(
-            vector.unsqueeze(1),
-            vector.unsqueeze(0),
-            dim=2
-        )
